d728c82a17f14715fde016c271f311262ddf6b4a
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-09 16:11:17 -0300

    add heroku domain to allowed hosts

--- bookhub/settings.py
+++ bookhub/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-pdbnofb=ksj$rn=9_%8zl^if22chmikb72*!)9jex)_ov9f@&0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['booksumioshi-074e801758a3.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
