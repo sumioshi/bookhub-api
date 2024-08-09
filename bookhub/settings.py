03e565f026c0a0a4c41a0ec410ca4a5670bc6a60
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-09 16:55:06 -0300

    Fix database settings

--- bookhub/settings.py
+++ bookhub/settings.py
@@ -90,13 +90,14 @@ WSGI_APPLICATION = "bookhub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
