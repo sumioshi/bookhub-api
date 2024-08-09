b8c1ac8317e9fcba023d749169a098d06c1a9e1e
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-09 16:22:52 -0300

    Configured Whitenoise to serve static files

--- bookhub/settings.py
+++ bookhub/settings.py
@@ -57,13 +57,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ROOT_URLCONF = "bookhub.urls"
 
 TEMPLATES = [
